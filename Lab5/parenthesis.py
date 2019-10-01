520473ca47a53747a621e58e9ad1c9a6dd7b5d89
obedjunias <obedjunias.cs17@bmsce.ac.in>
2019-10-01 14:33:55 +0530

    parenthesis completed

--- Lab5/parenthesis.py
+++ Lab5/parenthesis.py
@@ -1,36 +1,32 @@
-class Parentheses:
+
+class Parenthesis:
     def __init__(self):
-        self.o_list = []
-        self.c_list = []
-    def procesString(self,s):
-        for each in s:
-            if each == "(" or each == "{" or each == "[":
+        self.o_list=[]
+        self.popped=None
+    def isValid(self,str):
+        valid=False
+        for each in str:
+            if each == '(' or each=='{' or each== '[':
                 self.o_list.append(each)
-            else:
-                self.c_list.append(each)
-        
-        if len(self.o_list) != len(self.c_list):
-            print(self.o_list)
-            print(self.c_list)
-            print("Invalid List...")
-        else:
-            for i in range(len(self.o_list)-1,0,-1):
-                if self.o_list[i] == "(" and self.c_list[i] == ")":
-                    self.o_list.pop()
-                    self.c_list.pop()
-                elif self.o_list[i] == "{" and self.c_list[i] == "}":
-                    self.o_list.pop()
-                    self.c_list.pop()
-                elif self.o_list[i] == "[" and self.c_list[i] == "]":
-                    self.o_list.pop()
-                    self.c_list.pop()
+            elif each == ')' or each=='}' or each== ']':
+                if len(self.o_list) !=0 :
+                    self.popped = self.o_list.pop()
                 else:
-                    pass
+                    self.popped=None
+                if each == ')' and  self.popped == '(':
+                    valid=True
+                elif each == '}' and  self.popped=='{':
+                    valid =True
+                elif each == ']' and  self.popped =='[':
+                    valid = True
+                else:
+                    valid =False
+        if len(self.o_list) != 0:
+            valid =False
+        if(valid):
+            print("Valid string...")
+        else:
+            print("Invalid String...")
 
-            if len(self.o_list) == len(self.c_list) and len(self.o_list) == 0:
-                print("Valid...")
-            else:
-                print("InValid...")
-
-c = Parentheses()
-c.procesString("(){}")
+str=input("Enter string")
+Parenthesis().isValid(str)
